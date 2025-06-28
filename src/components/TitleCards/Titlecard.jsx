b3b38df63d13ae5d8f7b6ca8a8147155ee7ed37c
anukul rai <anukul.rau@gmail.com>
2025-06-28 19:09:06 +0545

    updates

---
+++ src/components/TitleCards/Titlecard.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Titlecard.css'
+
+
+function Titlecard() {
+  return (
+    <div className='titlecards'>
+      
+    </div>
+  )
+}
+
+export default Titlecard
